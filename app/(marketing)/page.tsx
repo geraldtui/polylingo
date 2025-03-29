ed36bdfb97702366fe4db07e77b3b89739cbb678
gerald.tui <gerald.tui@workday.com>
2025-03-30 01:14:17 +1300

    Added landing image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -7,33 +7,38 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0 center">
+        <Image src="/polylingolanding.svg" alt="PolyLingo Logo" fill />
+      </div>
       <div className="flex flex-col items-center gap-y-8">
-        <h1 className="text-xl lg:text-3xl font-bold text-netural-600 max-w-[480px] text-center">Learn, practice and master Pacific Languages with PolyLingo!</h1>
-      </div>
-      <div>
-        <ClerkLoading>
-          <Loader className="h-5 2-5 text-muted-foreground animated-spin"></Loader>
-        </ClerkLoading>
-        <ClerkLoaded>
-          <SignedOut>
-            <SignUpButton mode="modal" fallbackRedirectUrl="/learn">
-              <Button size="lg" variant="secondary" className="w-full">
-                Get Started
+        <h1 className="text-xl lg:text-3xl font-bold text-netural-600 max-w-[480px] text-center">Learn, practice and master Pacific Languages with PolyLingo!
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 2-5 text-muted-foreground animated-spin"></Loader>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" fallbackRedirectUrl="/learn">
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal" fallbackRedirectUrl="/learn">
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue Learning </Link>
               </Button>
-            </SignUpButton>
-            <SignInButton mode="modal" fallbackRedirectUrl="/learn">
-              <Button size="lg" variant="primaryOutline" className="w-full">
-                I already have an account
-              </Button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <Button size="lg" variant="secondary" className="w-full" asChild>
-              <Link href="/learn">Continue Learning </Link>
-            </Button>
-          </SignedIn>
-        </ClerkLoaded>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
       </div>
+
     </div>
   )
 }
