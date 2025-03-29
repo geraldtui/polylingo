3f2247ada803bc10a5a69fc4ba59daaa778480d3
gerald.tui <gerald.tui@workday.com>
2025-03-30 01:38:08 +1300

    Added Footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-between h-full gap-2">
+                <Button size="lg" variant="ghost" className="flex-1">
+                    <Image src="/ws.svg" alt="Samoan" width={40} height={32} className="mr-4 rounded-md" />
+                    Samoan
+                </Button>
+                <Button className="flex-1" size="lg" variant="ghost">
+                    <Image src="/fj.svg" alt="Fijian" width={40} height={32} className="mr-4 rounded-md" />
+                    Fijian
+                </Button>
+                <Button className="flex-1" size="lg" variant="ghost">
+                    <Image src="/to.svg" alt="Tongan" width={40} height={32} className="mr-4 rounded-md" />
+                    Tongan
+                </Button>
+                <Button className="flex-1" size="lg" variant="ghost">
+                    <Image src="/nu.svg" alt="Niuean" width={40} height={32} className="mr-4 rounded-md" />
+                    Niuean
+                </Button>
+                <Button className="flex-1" size="lg" variant="ghost">
+                    <Image src="/ck.svg" alt="Cook Island" width={40} height={32} className="mr-4 rounded-md" />
+                    Cook Island
+                </Button>
+            </div>
         </footer>
     )
 }
